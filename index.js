affd978ea0a172a344e77669cf3fc0c34ede60fa
James Campbell <james@supmenow.com>
2016-08-27 18:16:11 +0100

    Fallback.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 #!/usr/bin/env node
+var program = require('commander');
+
+program
+.version('0.0.1')
+
+program
+.command('init')
+.action(function () {
+   console.log('init')
+});
+
+program
+.command('start')
+.action(function () {
+   console.log('start')
+});
+
+program
+.command('test')
+.action(function () {
+   console.log('test')
+});
+
+program
+.action(function(){
+  program.outputHelp()
+});
+
+program.parse(process.argv);
